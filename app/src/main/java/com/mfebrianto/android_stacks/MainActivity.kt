c3ad4a687b5d298dd88c56b969277afd0eb234db
michael febrianto <michaelfebrianto@gmail.com>
2020-02-07 20:23:04 +1100

    first commit

---
+++ app/src/main/java/com/mfebrianto/android_stacks/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mfebrianto.android_stacks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
